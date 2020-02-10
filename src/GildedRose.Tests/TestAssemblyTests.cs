7885ed79fe1cef3a8a72e46d2a4883225752a685
Aldo Rafael Matus Angulo <aldo.matus@nearshoretechnology.com>
2020-02-10 10:53:13 -0600

    Added New Tests for 'Backstage Passes' items quality value.

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -100,6 +100,68 @@ namespace GildedRose.Tests
 
             Assert.Equal(cakeOldQuality, cakeNewQuality);
         }
+		
+		[Fact]
+        public void BackstagePassesShouldIncreaseQualityByOneWhenSellInIsGreaterThan10()
+        {
+            var program = new Program();
+            program.Items = Items;
+            var bsPassesOldQuality = Items.FirstOrDefault(item => item.Name.StartsWith("Backstage passes"))?.Quality;
 
+            program.UpdateQuality();
+
+            var bsPassesNewQuality = program.Items.FirstOrDefault(item => item.Name.StartsWith("Backstage passes"))?.Quality;
+
+            Assert.True(bsPassesNewQuality == (bsPassesOldQuality + 1));
+        }
+
+        [Fact]
+        public void BackstagePassesShouldIncreaseQualityByTwoWhenSellInIsLowerThan11()
+        {
+            var program = new Program();
+            Items.Find(x => x.Name.StartsWith("Backstage passes")).SellIn = 10;
+            
+            program.Items = Items;
+
+            var bsPassesOldQuality = Items.FirstOrDefault(item => item.Name.StartsWith("Backstage passes"))?.Quality;
+
+            program.UpdateQuality();
+
+            var bsPassesNewQuality = program.Items.FirstOrDefault(item => item.Name.StartsWith("Backstage passes"))?.Quality;
+
+            Assert.True(bsPassesNewQuality == (bsPassesOldQuality + 2));
+        }
+
+        [Fact]
+        public void BackstagePassesShouldIncreaseQualityByThreeWhenSellInIsLowerThan6()
+        {
+            var program = new Program();
+            Items.Find(x => x.Name.StartsWith("Backstage passes")).SellIn = 5;
+
+            program.Items = Items;
+
+            var bsPassesOldQuality = Items.FirstOrDefault(item => item.Name.StartsWith("Backstage passes"))?.Quality;
+
+            program.UpdateQuality();
+
+            var bsPassesNewQuality = program.Items.FirstOrDefault(item => item.Name.StartsWith("Backstage passes"))?.Quality;
+
+            Assert.True(bsPassesNewQuality == (bsPassesOldQuality + 3));
+        }
+
+        [Fact]
+        public void BackstagePassesShouldDropToZeroWhenSellInIs0()
+        {
+            var program = new Program();
+            Items.Find(x => x.Name.StartsWith("Backstage passes")).SellIn = 0;
+
+            program.Items = Items;
+
+            program.UpdateQuality();
+
+            var bsPassesNewQuality = program.Items.FirstOrDefault(item => item.Name.StartsWith("Backstage passes"))?.Quality;
+
+            Assert.True(bsPassesNewQuality == 0);
+        }
     }
 }
